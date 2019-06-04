68ed2e98f834408449d7061addb6d609dd03bef0
uzushino <torattoria.mamiya@gmail.com>
2019-06-04 12:19:04 +0900

    Fix main.rs

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,10 +1,13 @@
-use tokio;
+use std::sync::Arc;
+use recipro_engine::{ Engine, Isolate, Platform };
 
-mod loader;
+fn main() -> Result<(), failure::Error> {
+    let mut platform = Platform::new();
+    let engine = Arc::new(Isolate::new());
+    platform.add_engine(engine.clone());
 
-use loader::Loader;
+    engine.execute_script("a = 1 + 1;")?;
+    engine.execute_script("Recipro.log(a);")?;
 
-fn main()  {
-    let server = Loader::new();
-    tokio::run(server);
+    Ok(())
 }
